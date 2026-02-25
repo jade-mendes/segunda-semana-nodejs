c7d7d54c6e82b4f14674b519218a8fa100480e4b
Jade <jdmsa3@gmail.com>
2026-02-25 18:02:26 -0300

    feat: criando seeder

---
+++ prisma/seed.ts
@@ -0,0 +1,32 @@
+import { env } from "@/env/index.js";
+import { prisma } from "@/libs/prisma.js";
+import { hash } from "bcryptjs";
+
+export async function seed() {
+  await prisma.user.upsert({
+    where: {
+      email: "admin@email.com",
+    },
+    update: {},
+    create: {
+      publicId: "00000000-0000-0000-0000-000000000001",
+      name: "Administrador",
+      email: "admin@email.com",
+      passwordHash: await hash("senha", env.HASH_SALT_ROUNDS),
+      role: "ADMIN",
+    },
+  });
+
+  console.log("Database seeded successfully.");
+}
+
+seed()
+    .then(() => {
+        prisma.$disconnect()
+        process.exit(0)
+    })
+    .catch((error) => {
+        console.error("Error seeding database:", error)
+        prisma.$disconnect()
+        process.exit(1)
+    })
