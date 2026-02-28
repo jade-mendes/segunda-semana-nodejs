fd7078133657e70c230edec9c6aa46d874a835b5
Jade <jdmsa3@gmail.com>
2026-02-28 17:52:08 -0300

    feat: repositório de likes

---
+++ src/repositories/likes-repository.ts
@@ -0,0 +1,10 @@
+import type { Prisma, Like } from "@/@types/prisma/client.js";
+
+export interface ILikesRepository {
+  create(data: Prisma.LikeUncheckedCreateInput): Promise<Like>;
+  findBy(where: Prisma.LikeWhereInput): Promise<Like | null>;
+  findByUser(id: number): Promise<Like[]>;
+  findByPost(id: number): Promise<Like[]>;
+  delete(id: number): Promise<void>;
+  update(id: number, data: Prisma.LikeUpdateInput): Promise<Like>;
+}
---
+++ src/repositories/prisma/prisma-likes-repository.ts
@@ -0,0 +1,38 @@
+import type { Prisma } from "@/@types/prisma/client.js";
+import { prisma } from "@/libs/prisma.js";
+import type { ILikesRepository } from "../likes-repository.js";
+
+export class PrismaLikesRepository implements ILikesRepository {
+  async create(data: Prisma.LikeUncheckedCreateInput) {
+    return await prisma.like.create({ data });
+  }
+
+  async delete(id: number) {
+    await prisma.like.delete({
+      where: { id },
+    });
+  }
+
+  async findBy(where: Prisma.LikeWhereInput) {
+    return await prisma.like.findFirst({ where });
+  }
+
+  async update(id: number, data: Prisma.LikeUpdateInput) {
+    return await prisma.like.update({
+      where: { id },
+      data,
+    });
+  }
+
+  async findByUser(id: number) {
+    return await prisma.like.findMany({
+      where: { userId: id },
+    });
+  }
+
+  async findByPost(id: number) {
+    return await prisma.like.findMany({
+      where: { postId: id },
+    });
+  }
+}
